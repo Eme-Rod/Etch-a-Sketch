a972629d79c73ed8b393efb0f81aa9fb74529227
Rodrigo <rodrigo070duarte@gmail.com>
2022-03-01 12:17:27 -0300

    implemented a button to set a grid depending on the size specified by the user

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const container = document.querySelector("#container");
+const buttonGrid = document.createElement('button');
+const grid = document.createElement('div');
 
-for (let i = 0; i < 16; i++) {
-    const rowDIV = document.createElement('div');
-    rowDIV.classList.add('rowDIV');
-    for (let i = 0; i < 16; i++) {
-        const squareDIV = document.createElement('div');
-        squareDIV.classList.add('squareDIV');
+grid.setAttribute('id', 'grid');
+buttonGrid.setAttribute('id', 'btn-grid');
+buttonGrid.textContent = 'Clear';
+container.appendChild(buttonGrid);
+container.appendChild(grid);
 
-        squareDIV.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = "red";
-        })
-        rowDIV.appendChild(squareDIV);
-    }
-    container.appendChild(rowDIV);
+function btn_clear() {
+    const allGrid = document.querySelectorAll('.rowDIV');
+    allGrid.forEach(element => {
+        grid.removeChild(element);
+    });
+
+    const newGrid = prompt("What is the size of the new grid?");
+    gridSize(parseInt(newGrid))
 }
+
+function gridSize(size) {
+    for (let i = 0; i < size; i++) {
+        const rowDIV = document.createElement('div');
+        rowDIV.classList.add('rowDIV');
+        for (let i = 0; i < size; i++) {
+            const squareDIV = document.createElement('div');
+            squareDIV.classList.add('squareDIV');
+
+            squareDIV.addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = "red";
+            })
+            rowDIV.appendChild(squareDIV);
+        }
+    grid.appendChild(rowDIV);
+    }
+}
+
+buttonGrid.addEventListener('click', (e) => {
+    btn_clear();
+})
